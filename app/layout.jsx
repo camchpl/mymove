8e2aa58a79a776c1a5135ac8c5cd775f2c9dc6c3
Camille Chapleau <40691080+camchpl@users.noreply.github.com>
2024-03-10 17:23:41 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,8 +4,8 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'My Move',
-  description: 'inclusive Fitness App',
+  title: 'Let's move together',
+  description: 'Inclusive Fitness App',
 };
 
 export default function RootLayout({ children }) {
